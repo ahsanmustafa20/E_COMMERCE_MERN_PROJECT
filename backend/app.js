a99e34d167d56b50adcb9620c4724f97a67f455b
ahsanmustafa20 <ahsanmustafakamal@gmail.com>
2026-07-23 10:45:32 +0500

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+app.set("query parser", "extended");
+
+const errorMiddleware = require('./middleware/error');
+
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes import
+const product = require('./routes/productRoute');
+const user = require('./routes/userRoute');
+
+app.use('/api/v1', product);
+
+app.use("/api/v1", user);
+
+// Middleware for errors
+app.use(errorMiddleware);
+
+module.exports = app;
